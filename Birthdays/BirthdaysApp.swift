fd02f84fdaa2cf067b2126842542e5e14fa63a90
munaga-venkatesh <munagavenkatesh02@gmail.com>
2026-05-21 13:22:20 +0530

    Initial Commit

---
+++ Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Venkatesh Munaga on 21/05/26.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
